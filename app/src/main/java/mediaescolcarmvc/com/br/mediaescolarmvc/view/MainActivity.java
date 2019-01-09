b72ff04e82906245f32d171f2ac8d4436cc385ac
HalineGabriel <gabriel.moreeira@gmail.com>
2019-01-09 20:46:41 -0200

    Adicionado pasta referente ao fragmento

--- app/src/main/java/mediaescolcarmvc/com/br/mediaescolarmvc/view/MainActivity.java
+++ app/src/main/java/mediaescolcarmvc/com/br/mediaescolarmvc/view/MainActivity.java
@@ -107,9 +107,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_resultado_final) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_bimestre_a) {
 
         } else if (id == R.id.nav_share) {
 
